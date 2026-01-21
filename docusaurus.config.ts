21b5e3b5d4d07b38ed200633a79bdbb8268498ac
Aleksandar Zgonjan <acosoft@gmail.com>
2026-01-21 02:49:59 +0100

    Minor visual changes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,10 +69,13 @@ const config: Config = {
   ],
 
   themeConfig: {
+    
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
-      respectPrefersColorScheme: true,
+      defaultMode: 'light',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
     },
     navbar: {
       title: 'TimeOFF',
